8f495a042a64bd441e62485382e0b46bcf560d51
elagrga-tn <ela.grga@true-north.hr>
2025-01-17 14:03:52 +0100

    lets conflict

--- conflict.sql
+++ conflict.sql
@@ -1,8 +1,8 @@
-SELECT name, age WHERE age > 30;
+SELECT name, age WHERE age = 30;
 -----------------------------------
 SELECT nam, age FROM employees;
 -----------------------------------
-SELECT * FROM employees WHERE age => 30;
+SELECT * FROM employees WHERE age > 30;
 -----------------------------------
 SELECT * FROM employees WHERE name = John;
 -----------------------------------
